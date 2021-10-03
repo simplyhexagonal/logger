a16a07745315eecefc457a99c7b51248ef2a62f6
Jean M. Lescure <jeanmlescure@gmail.com>
2021-10-02 23:58:17 -0600

    chore: configure release it and dist

--- runkit.js
+++ runkit.js
@@ -3,3 +3,9 @@ const Logger = require('@simplyhexagonal/logger');
 const logger = new Logger({});
 
 logger.debug('Trying to teach', 2, 'tooters', {to: 'toot'});
+
+try {
+  throw new Error('you shall not pass');
+} catch {
+  logger.error('time to turn around');
+}
